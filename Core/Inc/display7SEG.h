9e4d15c13733b62f7c8fe0c79d2221f01c350e69
minhlang102 <minhlangstudy@gmail.com>
2022-10-07 15:04:58 +0700

    EX1_v2

---
+++ Core/Inc/display7SEG.h
@@ -0,0 +1,15 @@
+/*
+ * display7SEG.h
+ *
+ *  Created on: Oct 7, 2022
+ *      Author: minhl
+ */
+
+#ifndef INC_DISPLAY7SEG_H_
+#define INC_DISPLAY7SEG_H_
+
+#include "main.h"
+
+void display7SEG(int8_t counter);
+
+#endif /* INC_DISPLAY7SEG_H_ */
